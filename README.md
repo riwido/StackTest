d1e3beaa888b64f4fc02be8552f622c146a67437
zohassadar <zohassadar@gmail.com>
2024-06-17 17:52:02 +0000

    Serve async with uvicorn

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 * ~~Create react app~~
 * ~~Create fastapi app~~
 * ~~Button that pulls something from server~~
-* serve fastapi as async function
+* ~~serve fastapi as async function~~
 * add websockets
 * Send random words to client
 
--- src/stacktest/__main__.py
+++ src/stacktest/__main__.py
@@ -1,6 +1,26 @@
 import pathlib
+import asyncio
+from time import sleep
+import uvicorn
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+async def sleep_n_log():
+    try:
+        while True:
+            logger.info("Sleeping process doing something")
+            await asyncio.sleep(5)
+    except asyncio.CancelledError:
+        logger.info("Done sleeping")
 
-static = pathlib.Path(__file__).parent / "static"
 
-for file in static.iterdir():
-    print(f"Static file: {file}")
+async def main():
+    config = uvicorn.Config("stacktest.main:app", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    try:
+        await asyncio.gather(server.serve(), sleep_n_log())
+    except asyncio.CancelledError:
+        logger.info("Done serving")
+
+if __name__ == "__main__":
+    asyncio.run(main())
