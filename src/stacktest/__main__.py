aaa2531c521edc5bc7d2b8e71e7746365de154e5
zohassadar <zohassadar@gmail.com>
2024-06-18 04:23:40 +0000

    npm run dev from python

--- src/stacktest/__main__.py
+++ src/stacktest/__main__.py
@@ -1,11 +1,18 @@
+import asyncio
 import argparse
 import pathlib
 import uvicorn
+import sys
 import logging
+import subprocess
+
 from uvicorn.supervisors import ChangeReload
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+asyncio
+
 
 
 def main():
@@ -14,6 +21,7 @@ def main():
     args = parser.parse_args()
     logger.info(f"Args: {args}")
     if args.dev:
+        watcher = subprocess.Popen(['npm', 'run', 'dev'], stdout=sys.stdout, stderr=subprocess.STDOUT)
         config = uvicorn.Config(
             "stacktest.main:app",
             port=5000,
@@ -25,6 +33,7 @@ def main():
         server = uvicorn.Server(config)
         sock = config.bind_socket()
         ChangeReload(config, target=server.run, sockets=[sock]).run()
+        watcher.kill()
     else:
         config = uvicorn.Config(
             "stacktest.main:app",
