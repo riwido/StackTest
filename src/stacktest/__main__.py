99292389d565aa23c559ed93c718646509c72437
zohassadar <zohassadar@gmail.com>
2024-06-18 04:11:39 +0000

    Remove unnecessary hacks

--- src/stacktest/__main__.py
+++ src/stacktest/__main__.py
@@ -1,22 +1,39 @@
+import argparse
 import pathlib
 import uvicorn
-from uvicorn.supervisors import ChangeReload
 import logging
+from uvicorn.supervisors import ChangeReload
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+
 def main():
-    config = uvicorn.Config(
-        "stacktest.main:app",
-        port=5000,
-        log_level="debug",
-        reload=True,
-        reload_dirs=[str(pathlib.Path(__file__).parent)],
-    )
-    server = uvicorn.Server(config)
-    sock = config.bind_socket()
-    ChangeReload(config, target=server.run, sockets=[sock]).run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dev", action="store_true", help="Run in dev mode")
+    args = parser.parse_args()
+    logger.info(f"Args: {args}")
+    if args.dev:
+        config = uvicorn.Config(
+            "stacktest.main:app",
+            port=5000,
+            log_level="debug",
+            reload=True,
+            reload_dirs=[str(pathlib.Path(__file__).parent)],
+        )
+        logger.warning("Watching Enabled!!")
+        server = uvicorn.Server(config)
+        sock = config.bind_socket()
+        ChangeReload(config, target=server.run, sockets=[sock]).run()
+    else:
+        config = uvicorn.Config(
+            "stacktest.main:app",
+            port=5000,
+            log_level="info",
+        )
+        server = uvicorn.Server(config)
+        server.run()
+
 
 if __name__ == "__main__":
     try:
--- src/stacktest/main.py
+++ src/stacktest/main.py
@@ -13,39 +13,22 @@ from fastapi import WebSocket
 from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException
 from starlette.websockets import WebSocketDisconnect
-from uvicorn import Server
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
 static = pathlib.Path(__file__).parent / "static"
-
-
-"""
-https://stackoverflow.com/questions/58133694/graceful-shutdown-of-uvicorn-starlette-app-with-websockets
-"""
-handle_exit = Server.handle_exit
-_app_has_exited = False
-
-
-def patched_handle_exit(*args, **kwargs):
-    global _app_has_exited
-    _app_has_exited = True
-    handle_exit(*args, **kwargs)
-
-
-Server.handle_exit = patched_handle_exit
-
 
 class SPAStaticFiles(StaticFiles):
     async def get_response(self, path: str, scope):
         try:
             return await super().get_response(path, scope)
-        except HTTPException as ex:
+        except HTTPException as exc:
             if ex.status_code == 404:
                 logger.critical(f"404: {path=}")
                 return await super().get_response("404.html", scope)
             else:
-                raise ex
+                raise exc
 
 async def sleep_n_log():
     try:
@@ -58,29 +41,23 @@ async def sleep_n_log():
 
 @asynccontextmanager
 async def lifetime(app: FastAPI):
-    logger.info("beginning of timetime")
+    logger.info(f"beginning of lifetime for {app=}")
     task = asyncio.create_task(sleep_n_log())
     yield
     task.cancel()
-    await task
-    logger.info("end of timetime")
+    logger.info("end of lifetime")
 
 app = FastAPI(lifespan=lifetime)
 
 @app.get("/data")
 async def data():
-
     return dict(data=f"Server Uptime: {time.time() - then:.02f} seconds")
-
 
 @app.websocket("/ws")
 async def ws(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
-            if _app_has_exited:
-                logger.info("Cancelling websocket loop")
-                break
             await websocket.send_text(
                 "".join(random.choice(string.ascii_uppercase) for _ in range(10))
             )
